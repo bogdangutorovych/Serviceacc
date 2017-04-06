f7fe76acd19a3ab5793b030f6685e54c33c862f0
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-04-07 01:16:03 +0300

    create JsfConfig

---
+++ src/main/java/ua/com/foxminded/serviceacc/config/JsfConfig.java
@@ -0,0 +1,27 @@
+package ua.com.foxminded.serviceacc.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.faces.webapp.FacesServlet;
+
+/**
+ * Created by andreb on 07.04.17.
+ */
+@Configuration
+public class JsfConfig {
+
+    @Bean
+    public FacesServlet facesServlet() {
+        return new FacesServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration =
+                new ServletRegistrationBean(facesServlet(), "*.xhtml");
+        registration.setName("FacesServlet");
+        return registration;
+    }
+}
