23d40b2921c9b660c0d5158db34312f4ae3645d2
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-04-08 10:41:40 +0300

    fix /

---
+++ src/main/java/ua/com/foxminded/serviceacc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package ua.com.foxminded.serviceacc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by andreb on 08.04.17.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home(ModelMap map) {
+        return "client/clientTemplate.xhtml";
+    }
+}
