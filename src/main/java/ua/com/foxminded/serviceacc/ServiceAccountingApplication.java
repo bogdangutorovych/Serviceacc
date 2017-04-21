5bf4c8ec4ef2288fe6f0d7e484cf893c8b3a9280
Viacheslav Zhevaha <v.zhevaga@gmail.com>
2017-04-22 00:30:34 +0300

    fix merge with origin jenkins

--- src/main/java/ua/com/foxminded/serviceacc/ServiceAccountingApplication.java
+++ src/main/java/ua/com/foxminded/serviceacc/ServiceAccountingApplication.java
@@ -2,6 +2,8 @@ package ua.com.foxminded.serviceacc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
