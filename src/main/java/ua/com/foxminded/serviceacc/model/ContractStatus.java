b42f2d3218da6e2cf93998e35381e92ccb311023
Viacheslav Zhevaha <v.zhevaga@gmail.com>
2017-04-27 23:17:00 +0300

    add ContractStatus

---
+++ src/main/java/ua/com/foxminded/serviceacc/model/ContractStatus.java
@@ -0,0 +1,96 @@
+package ua.com.foxminded.serviceacc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * Created by andreb on 04.04.17. Represent Client title
+ */
+@Entity
+@Table(name = "client_status_type")
+public class ContractStatus {
+	@Id
+	@GenericGenerator(name = "generator", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+			@Parameter(name = "sequence_name", value = "client_status_type_id_seq"),
+			@Parameter(name = "initial_value", value = "1"), @Parameter(name = "increment_size", value = "1") })
+	@GeneratedValue(generator = "generator")
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
+
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
+
+	@Column(name = "title", nullable = false)
+	private String title;
+
+	@Column(name = "active", nullable = false)
+	private boolean active = true;
+
+	public ContractStatus() {
+	}
+
+	public ContractStatus(String code, String title) {
+		this.code = code;
+		this.title = title;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return title;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		ContractStatus that = (ContractStatus) o;
+
+		return id != null ? id.equals(that.id) : that.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+}
