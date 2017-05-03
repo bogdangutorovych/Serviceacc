ae21d5ef804464f5899a4ec25ae0515224cfaaad
Bogdan Gutorovych <bogdan.gutorovych@gmail.com>
2017-05-03 13:19:34 +0300

    fix output date

--- src/main/java/ua/com/foxminded/serviceacc/converter/LocalDateConverterJsf.java
+++ src/main/java/ua/com/foxminded/serviceacc/converter/LocalDateConverterJsf.java
@@ -8,18 +8,18 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-@FacesConverter(value="localDateTimeConverter")
-public class LocalDateTimeConverter implements Converter {
+@FacesConverter(value="localDateConverter")
+public class LocalDateConverterJsf implements Converter {
 
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        return LocalDate.parse(value);
+        return LocalDate.parse(value, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
         LocalDate dateValue = (LocalDate) value;
-        return dateValue.format(DateTimeFormatter.ofPattern("dd/MM/yy"));
+        return dateValue.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
 }
