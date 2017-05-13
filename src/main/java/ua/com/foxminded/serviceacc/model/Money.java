f420ff6fca8e2c333cca629b11da07b96a595abd
Andrey Bobrow <aquariusmaster@yandex.ru>
2017-05-14 00:00:40 +0300

    #38

--- src/main/java/ua/com/foxminded/serviceacc/model/Money.java
+++ src/main/java/ua/com/foxminded/serviceacc/model/Money.java
@@ -7,15 +7,23 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Loader;
 import org.hibernate.annotations.Parameter;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import ua.com.foxminded.serviceacc.model.enums.CurrencyType;
 
 @Entity
 @Table(name = "money")
+@SQLDelete(sql = "UPDATE money SET active = false WHERE id = ?")
+@Loader(namedQuery = "findMoneyById")
+@NamedQuery(name = "findMoneyById", query = "FROM Money WHERE id = ?1 AND active = true")
+@Where(clause = "active = true")
 public class Money {
 
     @Id
@@ -74,5 +82,29 @@ public class Money {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        if (currency != money.currency) return false;
+        return amount != null ? amount.equals(money.amount) : money.amount == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = currency != null ? currency.hashCode() : 0;
+        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
     }
 }
--- src/main/java/ua/com/foxminded/serviceacc/model/Service.java
+++ src/main/java/ua/com/foxminded/serviceacc/model/Service.java
@@ -1,77 +1,123 @@
 package ua.com.foxminded.serviceacc.model;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Loader;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "service")
+@SQLDelete(sql = "UPDATE service SET active = false WHERE id = ?")
+@Loader(namedQuery = "findServiceById")
+@NamedQuery(name = "findServiceById", query = "FROM Service WHERE id = ?1 AND active = true")
+@Where(clause = "active = true")
 public class Service {
 
-    @Id
-    @GenericGenerator(name = "generator", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
-            @Parameter(name = "sequence_name", value = "service_id_seq"),
-            @Parameter(name = "initial_value", value = "1"), @Parameter(name = "increment_size", value = "1") })
-    @GeneratedValue(generator = "generator")
-    @Column(name = "id", unique = true, nullable = false)
-    private Long id;
+	@Id
+	@GenericGenerator(name = "generator", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+			@Parameter(name = "sequence_name", value = "service_id_seq"),
+			@Parameter(name = "initial_value", value = "1"), @Parameter(name = "increment_size", value = "1") })
+	@GeneratedValue(generator = "generator")
+	@Column(name = "id", unique = true, nullable = false)
+	private Long id;
 
-    @Column(name = "name")
-    private String name;
+	@Column(name = "name")
+	private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "money_id")
-    private Money price;
+    @Column(name = "description")
+    private String description;
 
-    @Column(name = "active", nullable = false)
-    private boolean active = true;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private Set<Money> prices = new HashSet<>();
 
-    public Service() {
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "rate_id")
+    private Money managerRate = new Money();
 
+	@Column(name = "active", nullable = false)
+	private boolean active = true;
+
+	public Service() {
+
+	}
+
+	public Service(String name, Set<Money> prices) {
+		this.name = name;
+		this.prices = prices;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    public String getDescription() {
+        return description;
     }
 
-    public Service(String name,String description, Money price) {
-        this.name = name;
-        this.price = price;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public long getId() {
-        return id;
+    public Set<Money> getPrices() {
+		return prices;
+	}
+
+	public void setPrices(Set<Money> prices) {
+		this.prices = prices;
+	}
+
+    public Money getManagerRate() {
+        return managerRate;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Money getPrice() {
-        return price;
-    }
-
-    public void setPrice(Money price) {
-        this.price = price;
+    public void setManagerRate(Money managerRate) {
+        this.managerRate = managerRate;
     }
 
     public boolean isActive() {
-        return active;
-    }
+		return active;
+	}
 
-    public void setActive(boolean active) {
-        this.active = active;
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+    @Override
+    public String toString() {
+        return "Service{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", description='" + description + '\'' +
+            ", prices=" + prices +
+            ", managerRate=" + managerRate +
+            ", active=" + active +
+            '}';
     }
 }
